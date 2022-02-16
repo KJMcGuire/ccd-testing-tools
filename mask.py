6351434caf05142e24f521c284ed4be972c6c442
KJ McGuire <kellie@kelliejensen.com>
2022-02-15 18:16:56 -0800

    Add plots

--- mask.py
+++ mask.py
@@ -14,10 +14,13 @@ import numpy.ma as ma
 from scipy import stats
 import argparse
 import ROOT
+from collections import namedtuple
+
+NamedArray = namedtuple("NamedArray", "name data")
 
 
 ###Compute mean of multi-skip images; stack imgs by row
-def avg_skp(fitsfiles, n_amps):
+def avg_skp(fitsfiles, n_amps, skp_img):
 
     shape = None
     stacked_imgs = None
@@ -32,68 +35,63 @@ def avg_skp(fitsfiles, n_amps):
         if n_amps == 2:
             data = np.split(data, 2, axis=1)
 
-        #data = np.reshape(data, (cols*skips,rows))
-        data = np.reshape(data, (cols, rows, skips))
-        # data = np.array(data)
-        # print(data.shape)
+        if skp_img:
+            data = np.reshape(data, (cols, rows, skips))
+            data = np.mean(data, axis=2)
+        else:
+            data = np.array(data)
+            data = np.flip(data.T, axis=1)
+        print(data.shape)
 
         #print(data.shape)
         if i==0:
             shape = data.shape
-            data = np.mean(data, axis=2)
+            # if skp_img:
+            #     data = np.mean(data, axis=2)
             stacked_imgs = data
         else:
             if data.shape != shape:
                 raise AssertionError("Fits files must be the same shape")
-            stacked_imgs = np.hstack((stacked_imgs, np.mean(data, axis=2)))
+            #stacked_imgs = np.hstack((stacked_imgs, np.mean(data, axis=2)))
+            stacked_imgs = np.hstack((stacked_imgs, data))
 
     return stacked_imgs  #2D array of stacked images
 
 
-###Calculate 1-D projections (sum, median, MAD) and plot results
+###Calculate 1-D projections (sum, median, MAD)
 def projection_x(stacked, gain):
 
     calibrated = stacked / gain
-    e_tot = np.sum(calibrated, axis = 1)
-    median = np.median(calibrated[0:3100,:], axis = 1)
-    median = np.amax(median)-median
-    #print(median)
-    MAD = stats.median_abs_deviation(calibrated[0:3100,:], axis = 1)
-
-    fig, ax = plt.subplots(figsize=(10, 5.4))
-    #print(MAD[110:120])
-    #ax.plot(e_tot)
-    #print(calibrated[1,:])
-    #ax.plot(MAD[110:120])
+    calibrated = np.amax(calibrated) - calibrated
+    e_tot = NamedArray("e_tot", np.sum(calibrated, axis = 1))
+    median = NamedArray("median", np.median(calibrated, axis = 1))
+    MAD = NamedArray("MAD", stats.median_abs_deviation(calibrated, axis = 1))
     return (e_tot, median, MAD)
 
 
-###Plot histograms and fit to poisson-gauss
-def histogram(data, bin_sz):
+def plot(data):
+    fig, ax = plt.subplots(figsize=(10, 5.4))
+    ax.plot(data.data, "k.")
+    ax.set_xlabel("Column number".format(data.name))
+    ax.set_ylabel("{} (ADU)".format(data.name))
+    plt.show()
+    return
 
-    range=(np.amin(data),np.amax(data))
-    bins = (np.amax(data)-np.amin(data))/bin_sz
-    hist, bin_edges = np.histogram(data,bins=int(bins),range=range)
-    bin_centers = 0.5*(bin_edges[1:]+bin_edges[:1])
-    plt.rcParams.update({'font.size': 15})
-    fig, ax = plt.subplots(figsize=(10,5.4))
-    #ax.errorbar(bin_centers, hist, fmt='k_', yerr=hist**0.5)
-    n = ax.hist(bin_edges[:-1],bin_edges,align='left',weights=hist,histtype='step')
-    return n
+
 
 ###Plot histogram and fit using ROOT
-def ROOThist(data, bin_sz,):
+def ROOThist(data, bin_sz, lam, norm):
 
     ROOT.gStyle.SetOptFit(111)
     c = ROOT.TCanvas("canvas")
 
-    n_bins = (np.amax(data)-np.amin(data))/bin_sz
-    print("max = {}".format(np.amax(data)))
-    hist = ROOT.TH1F("hist","hist", int(n_bins), np.amin(data), np.amax(data))
-    s = np.random.poisson(6,3100)
-    s = s*bin_sz
+    n_bins = (np.amax(data.data)-np.amin(data.data))/bin_sz
+    #n_bins=100/bin_sz
+    hist = ROOT.TH1F("hist","hist", int(n_bins), 0, np.amax(data.data))
+    #s = np.random.poisson(6,3100)
+    #s = s*bin_sz
 
-    for i in data:
+    for i in data.data:
         hist.Fill(i)
 
     """
@@ -108,16 +106,16 @@ def ROOThist(data, bin_sz,):
     #func = ROOT.TF1("poiss_gauss","TMath::Poisson(x,[2])*TMath::Gaus(x,[1],[2])")
 
     #func.SetParNames("norm","#lambda_{poisson}","scale")
-    func.SetParameters(3100,12,1/bin_sz)
+    func.SetParameters(norm, lam, 1/bin_sz)
 
-    hist.GetXaxis().SetTitle(" (ADU)")
+    hist.GetXaxis().SetTitle("{} (ADU)".format(data.name))
     hist.GetYaxis().SetTitle("No. Cols")
 
 
     hist.Fit(func)
     hist.Draw()
 
-    hist.GetXaxis().SetRangeUser(0,100)
+    #hist.GetXaxis().SetRangeUser(0,100)
     hist.SetTitle("[0]*TMath::Poisson(x*[2],[1])")
     #hist.SetCanExtend(ROOT.TH1.kAllAxes)
     #hist.ExtendAxis(10, hist.GetXaxis())
@@ -134,71 +132,46 @@ def ROOThist(data, bin_sz,):
     return (std, mean)
 
 
-def testROOT():
-    f1= ROOT.TF1("func1","sin(x)", 0, 10)
-    f1.Draw()
-    input("Press Enter ........ ")
-    return
-
-
-
 ###Generate mask
-def mask(data, stddev, mean):
+def make_mask(meds, MADs, MAD_std, MAD_mean, med_std, med_mean):
 
+    med_mask = np.ma.masked_where(np.logical_or(meds.data>med_mean+med_std*3,
+                            meds.data<med_mean-med_std*3),meds.data,copy=False)
+    MAD_mask = np.ma.masked_where(MADs.data>MAD_mean+MAD_std*1.75,MADs.data,copy=False)
+    mask = np.any((med_mask.mask, MAD_mask.mask), axis=0)
 
-    #print(data[110:120])
+    masked = mask.sum()
+    print("{} columns masked".format(masked))
 
-    #mask = np.where(condition[data>2])
-    mask = np.ma.masked_where(np.logical_or(data>mean+stddev*3,data<mean-stddev*3),data,copy=False)
-    #mask = np.ma.masked_where(data>mean+stddev*2,data,copy=False)
-    #mask_0 = np.ma.MaskedArray(data, mask=111)
-    #mask_0 = np.ma.MaskedArray(data, mask=np.ma.masked_where(data>mean+stddev*1,data,copy=True))
-
-    #print(mask[110:120])
-    #print(mask.mask[110:120])
-    #print(mask_0[110:120])
-
-    #print(mask0[100:200])
-    plt.rcParams.update({'font.size': 15})
-
-    plt.plot(mask.mask)
-    # plt.title("")
+    plt.imshow(np.expand_dims(mask, axis=0), aspect="auto", cmap='binary')
+    plt.title("{} columns masked".format(masked))
     plt.xlabel("Columns")
     plt.show()
     return mask
 
 
-
-
 def main():
-    parser = argparse.ArgumentParser(description='Compress multi-skip FITS image and fit single-e peaks.')
+    parser = argparse.ArgumentParser(description='Generate CCD Mask')
     parser.add_argument('-f','--files', metavar='FILES', type=str, help='FITS files',required=True, nargs='*')
     parser.add_argument('-s','--sigma', metavar='Noise',type=float,default=0.2, help='Noise parameter for single-e peaks fit')
     parser.add_argument('-l','--lamb', metavar='Dark current',type=float,default=0.01, help='Dark current parameter for single-e peaks fit')
     parser.add_argument('-g','--gain',metavar='CCD gain', type=float,default=1., help='ADU to electron conversion for single-e peaks fit')
     args = parser.parse_args()
 
-    stacked = avg_skp(args.files, n_amps=2)
-    e_tot, median, MAD = projection_x(stacked, args.gain)
-    #std, mean = ROOThist(MAD, 0.1)
-    #masked_MAD = mask(MAD, std, mean)
-    #ROOThist(masked_MAD, 0.1)
-    std, mean = ROOThist(median, 0.1)
-    masked_median = mask(median, std, mean)
-    ROOThist(masked_median, 0.1)
-
-    #histogram(median,1)
-    #plt.imshow(stacked, vmin=np.nanmean(stacked)-np.nanstd(stacked), vmax=np.nanmean(stacked)+np.nanstd(stacked), cmap="viridis")
-    #print(stacked.shape)
-    #plt.plot(figsize=(10, 5.4))
-    #plt.imshow(stacked.T, vmin=np.nanmean(stacked)-np.nanstd(stacked), vmax=np.nanmean(stacked)+np.nanstd(stacked), aspect='auto', cmap="viridis")
-
-    #plt.imshow(stacked.T[:,3200:-1], vmin=18640, vmax=18645, aspect='auto', cmap="gist_heat")
-    #plt.colorbar(label="ADU")
+    stacked = avg_skp(args.files, n_amps=2, skp_img=True)
+    e_tot, median, MAD = projection_x(stacked[3200:-1,:], args.gain)
+    plot(e_tot)
+    plot(median)
+    plot(MAD)
+    MAD_std, MAD_mean = ROOThist(MAD, 0.1, 12, 3100)
+    med_std, med_mean = ROOThist(median, 0.1, 12, 3100)
 
 
+    mask = make_mask(median, MAD, MAD_std, MAD_mean, med_std, med_mean)
+    #med_masked = np.ma.masked_array(median, mask=mask)
+    #MAD_masked = np.ma.masked_array(MAD, mask=mask)
+    #ROOThist(med_masked, 0.1)
 
-    plt.show()
 
 if __name__ == '__main__':
     main()
