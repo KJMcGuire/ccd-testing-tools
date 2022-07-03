60b891a0271709b885708424cd20cfbca359ff02
KJ McGuire <kellie@kelliejensen.com>
2022-07-03 12:14:08 -0700

    add - save imgs to root histos

--- median_imgs.py
+++ median_imgs.py
@@ -5,29 +5,27 @@
 ##
 ##Takes FITS files and computes median value for each
 ##pixel and median value after eliminating outliers.
-##Saves results to .txt files.
+##Saves results to root histograms.
 ######################################################
 
 
-from glob import glob
 import numpy as np
 from astropy.io import fits
-import os.path
-from os import path
 import argparse
 import sys
 import matplotlib.pyplot as plt
+import ROOT
 import time
 import datetime
 import numpy.ma as ma
 
 
 ###Create array of the data from FITS file
-def get_data(fitsfiles):
-    for i in range(len(fitsfiles)): 
+def get_data(fitsfiles,ext=0):
+    for i in range(len(fitsfiles)):
         hdulist = fits.open(fitsfiles[i])
         hdulist.info()
-        fitsfiles[i] = hdulist[0].data
+        fitsfiles[i] = hdulist[ext].data
         #if i == len(fitsfiles):
         #    hdulist.close()
     fitsfiles = np.array(fitsfiles)
@@ -48,20 +46,33 @@ def flatten_files(fitsfiles):
 ###all files. Creates arrays populated with median
 ###and median after omitting outliers and saves to .txt
 #########################################################
-def median_imgs(flatfiles, width, height):
+def median_imgs(flatfiles, outfile, width, height):
     print(flatfiles.shape)
     n_files, file_length = flatfiles.shape
 
     median_img = np.median(flatfiles, axis=0)
-    
+
     stddev = np.std(flatfiles,axis=0)
     masked = np.ma.masked_where(np.logical_or(flatfiles<median_img-stddev,flatfiles>median_img+stddev),flatfiles,copy=False)
-    #median_sans_outliers = np.ma.median(masked, axis=0)
+    median_sans_outliers = np.ma.median(masked, axis=0)
 
     median_img = median_img.reshape(width, height)
-    #median_sans_outliers = median_sans_outliers.reshape(width, height)
-    
-    np.savetxt('median_img.txt', median_img, delimiter=',')
+    median_sans_outliers = median_sans_outliers.reshape(width, height)
+
+    title = outfile+".root"
+    f = ROOT.TFile(title, "RECREATE")
+    h = ROOT.TH2D("med","median img", height, 0.5, height+0.5, width, 0.5, width+0.5)
+    h2 = ROOT.TH2D("sansout","median img sans outliers", height, 0.5, height+0.5, width, 0.5, width+0.5)
+
+    for i in range(height):
+        for j in range(width):
+            h.Fill(i+1, j+1, median_img[j,i])
+            h2.Fill(i+1, j+1, median_sans_outliers[j,i])
+
+    h.Write()
+    h2.Write()
+    f.Close()
+    #np.savetxt('median_img.txt', median_img, delimiter=',')
     #np.savetxt('median_sans_outliers.txt', median_sans_outliers, delimiter=',')
     return
 
@@ -69,33 +80,19 @@ def median_imgs(flatfiles, width, height):
 
 
 def main():
-#    if len(sys.argv)<2:
-#
-#        print("Error: first argument must be a FITS file")
-#        exit(1)
-#
-#    if path.exists(sys.argv[1]):
-#        if sys.argv[1].endswith('.fits'):
-#            fitsfiles = glob("*.fits")
-#            print(fitsfiles)
-#        else:
-#            print("Enter a valid FITS file")
-#            exit(1)
 
-#    else:
-#        print("Enter a valid FITS file")
-#        exit(1)
     parser = argparse.ArgumentParser(description='Generate median CCD img')
     parser.add_argument('-f','--files', metavar='FILES', type=str, help='FITS files',required=True, nargs='*')
+    parser.add_argument('-e','--ext', metavar='Fits Extention', type=int, default=0, help='Extension of FITS file')
     args = parser.parse_args()
 
 
     start_time = time.time()
 
-    data = get_data(args.files)
+    data = get_data(args.files, args.ext)
     flatfiles = flatten_files(data)
     n_files, width, height = data.shape
-    median_imgs(flatfiles,width,height)
+    median_imgs(flatfiles,"20220702_median",width,height)
 
 
     print(f"time: {datetime.timedelta(seconds=int(time.time() - start_time))}")
